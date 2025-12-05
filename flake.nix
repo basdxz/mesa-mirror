5f1400b47ebd2a16fcd11fb2acbf54c7064cb327
FalsePattern <me@falsepattern.com>
2025-12-05 17:51:03 +0100

    fp: use chaotic mesa_git deps

--- flake.nix
+++ flake.nix
@@ -1,5 +1,6 @@
 {
-  inputs.nixpkgs.url = "nixpkgs/4f92f68f31251e2a710f4619df1b9f6bf5983b0f";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.chaotic.url = "github:chaotic-cx/nyx/e84e243d10c138885b9d01aa1494b6b0cb6b1d32";
 
   outputs = {self, ...}@inputs:
   
@@ -15,16 +16,21 @@
         inputs.nixpkgs.lib.genAttrs supportedSystems (
           system:
           f {
-            pkgs = import inputs.nixpkgs { inherit system; };
+            pkgs = import inputs.nixpkgs {
+              inherit system;
+              overlays = [
+                inputs.chaotic.overlays.default
+              ];
+            };
           }
         );
     in {
     devShells = forEachSupportedSystem(
       {pkgs}: {
         default = pkgs.mkShell {
-            packages = pkgs.mesa.buildInputs ++ pkgs.mesa.nativeBuildInputs;
-            shellHook = pkgs.mesa.preConfigure;
-            mesonFlags = pkgs.mesa.mesonFlags;
+            packages = pkgs.mesa_git.buildInputs ++ pkgs.mesa_git.nativeBuildInputs;
+            shellHook = pkgs.mesa_git.preConfigure;
+            mesonFlags = pkgs.mesa_git.mesonFlags;
         };
       }
     );
